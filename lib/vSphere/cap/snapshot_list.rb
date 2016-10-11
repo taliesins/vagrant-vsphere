30850bb8c46345a33eb9b209cce0fff4884c01ac
Taliesin Sisson <taliesins@yahoo.com>
2016-10-11 12:31:38 +0100

    Use a lock per a machine for actions but ensure that it works with a single process

--- lib/vSphere/cap/snapshot_list.rb
+++ lib/vSphere/cap/snapshot_list.rb
@@ -2,12 +2,20 @@ module VagrantPlugins
   module VSphere
     module Cap
       module SnapshotList
+        @@lock = Mutex.new
+
         # Returns a list of the snapshots that are taken on this machine.
         #
         # @return [Array<String>] Snapshot Name
         def self.snapshot_list(machine)
-          env = machine.action(:snapshot_list, lock: false)
-          env[:machine_snapshot_list]
+
+          @@lock.synchronize do
+            lock_key = Digest::MD5.hexdigest(machine.box.name)
+            machine.env.lock(lock_key, retry: true) do
+              env = machine.action(:snapshot_list)
+              return env[:machine_snapshot_list]
+            end
+          end
         end
       end
     end
--- lib/vSphere/provider.rb
+++ lib/vSphere/provider.rb
@@ -3,31 +3,47 @@ require 'vagrant'
 module VagrantPlugins
   module VSphere
     class Provider < Vagrant.plugin('2', :provider)
+      @@lock = Mutex.new
+
       def initialize(machine)
         @machine = machine
       end
 
       def action(name)
-        action_method = "action_#{name}"
-        return Action.send(action_method) if Action.respond_to?(action_method)
-        nil
+        @@lock.synchronize do
+          lock_key = Digest::MD5.hexdigest(@machine.box.name)
+          @machine.env.lock(lock_key, retry: true) do
+            action_method = "action_#{name}"
+            return Action.send(action_method) if Action.respond_to?(action_method)
+            return nil
+          end
+        end
       end
 
       def ssh_info
-        env = @machine.action('get_ssh_info', lock: false)
-        env[:machine_ssh_info]
+        @@lock.synchronize do
+          lock_key = Digest::MD5.hexdigest(@machine.box.name)
+          @machine.env.lock(lock_key, retry: true) do
+            env = @machine.action('get_ssh_info')
+            return env[:machine_ssh_info]
+          end
+        end
       end
 
       def state
-        env = @machine.action('get_state', lock: false)
+        @@lock.synchronize do
+          lock_key = Digest::MD5.hexdigest(@machine.box.name)
+          @machine.env.lock(lock_key, retry: true) do
+            env = @machine.action('get_state')
+            state_id = env[:machine_state_id]
 
-        state_id = env[:machine_state_id]
+            short = "vagrant_vsphere.states.short_#{state_id}"
+            long  = "vagrant_vsphere.states.long_#{state_id}"
 
-        short = "vagrant_vsphere.states.short_#{state_id}"
-        long  = "vagrant_vsphere.states.long_#{state_id}"
-
-        # Return the MachineState object
-        Vagrant::MachineState.new(state_id, short, long)
+            # Return the MachineState object
+            return Vagrant::MachineState.new(state_id, short, long)            
+          end
+        end
       end
 
       def to_s
