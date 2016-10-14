41be96e6ba7a2d8a77f18f46bd91bb5fb6b37d1a
Taliesin Sisson <taliesins@yahoo.com>
2016-10-14 17:37:21 +0100

    Don't want a class level variable for connection as we want one per a machine

--- lib/vSphere/driver.rb
+++ lib/vSphere/driver.rb
@@ -5,31 +5,34 @@ require 'vSphere/util/vm_helpers'
 module VagrantPlugins
   	module VSphere
     	class Driver
-    		@@connection = nil
-
 			def initialize(machine)
 				 @machine = machine
+				 @connection = nil
+				 ObjectSpace.define_finalizer(self, self.class.finalize(@connection) )
 			end
 
-			def finalize
-				close_connection
+			def self.finalize(conn)
+				if conn 
+					conn.close 
+					conn = nil
+				end
 			end
 
 			def connection
-				return @@connection if @@connection
+				return @connection if @connection
 
 				config = @machine.provider_config
 
-				@@connection  = RbVmomi::VIM.connect host: config.host,
+				@connection  = RbVmomi::VIM.connect host: config.host,
 					user: config.user, password: config.password,
 					insecure: config.insecure, proxyHost: config.proxy_host,
 					proxyPort: config.proxy_port
 			end
 
 			def close_connection
-				if @@connection 
-					@@connection.close 
-					@@connection = nil
+				if @connection 
+					@connection.close 
+					@connection = nil
 				end
 			end
 
